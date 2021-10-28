d0ecce5f0012a597eec5841bbe2bfb11fb4fa061
Vladislav Cherdantsev <cherdantsewva@gmail.com>
2021-10-28 15:38:03 +0300

    increase stability, efficiency

--- src/main/java/app/Server.java
+++ src/main/java/app/Server.java
@@ -15,12 +15,12 @@ import java.util.logging.Logger;
 
 public class Server {
     private static final Logger LOGGER = Logger.getLogger(Server.class.getName());
+    private static final int BACKLOG = 500;
     private final ServerProperties serverProperties;
     private final RequestReader requestReader;
     private final ResponseWriter responseWriter;
     private final ResponseProvider responseProvider;
     private final AtomicInteger atomicInteger = new AtomicInteger(0);
-    private final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(100);
 
     public Server(ServerProperties serverProperties, RequestReader requestReader, ResponseWriter responseWriter, ResponseProvider responseProvider) {
         this.serverProperties = serverProperties;
@@ -30,15 +30,14 @@ public class Server {
     }
 
     public void start() throws IOException {
-        ServerSocket serverSocket = new ServerSocket(serverProperties.getPort(), 100);
+        ServerSocket serverSocket = new ServerSocket(serverProperties.getPort(), BACKLOG);
         LOGGER.log(Level.INFO, "Server started.");
-        Executor executorService = new CountingTimeThreadPoolExecutor(new ThreadPoolExecutor(20, 30, 0L, TimeUnit.MILLISECONDS, queue));
+        Executor executorService = Executors.newFixedThreadPool(20);
         while (true) {
             ConnectionHandler connectionHandler = new ConnectionHandler(requestReader, responseWriter, serverSocket.accept(), responseProvider, serverProperties);
             LOGGER.log(Level.INFO, "Client accepted.");
             System.out.println("Total accepted clients " + atomicInteger.incrementAndGet());
             executorService.execute(connectionHandler);
-            System.out.println("The queue size: " + queue.size());
         }
     }
 }
